f14cb50cd86632ff9a8f6f0f92e4048410b7dd57
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-01 16:18:07 +0800

    Finished Design of Login Page and Sign up Page"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show,:edit,:update,:delete]
+  # before_action :logged_in_user, only: [:show,:edit,:update,:delete]
   def new
     @user = User.new
   end
