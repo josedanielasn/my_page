cfa5d4b782da1ba1ef53c531e8d8ecbe4e65215f
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-07 21:33:57 +0800

    added tag_test file

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save empty tag_name" do
+    new_tag = Tag.new(tag_name:'educational')
+    assert_not new_tag.save, 'Saved with an empty tag'
+  end
 end
